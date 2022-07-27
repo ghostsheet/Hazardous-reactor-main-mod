6813149d73074e4152d2abff14c22de12cc54e9e
ghostsheet <43085828+ghostsheet@users.noreply.github.com>
2022-07-27 19:20:55 +1000

    Geiger Counter

---
+++ Hazardous Reactors [ Lethal radiation update  ]/Lua/Autorun/Geiger counter.lua
@@ -0,0 +1,18 @@
+local geigerRadsIndex = {}
+
+Hook.Add("geigercount", "geigereffect", function (effect, deltaTime, item, targets, worldPosition)
+  if targets[1] == nil then return end
+  if geigerRadsIndex[item] == nil then geigerRadsIndex[item] = 0 end
+
+  local c = targets[1]
+  local r = c.CharacterHealth.GetAffliction("radiationgeiger")
+  local rads = 0
+  if(r) then
+    rads = r.Strength
+  end
+
+  local deltaRads = math.max(rads - geigerRadsIndex[item], 0)
+  item.condition = 100 - (deltaRads * 5)
+  geigerRadsIndex[item] = rads
+
+end)
